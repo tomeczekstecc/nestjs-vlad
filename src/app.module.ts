d56572ba0c7d5549059535bf80d196627ce14ee4
Tomasz Steć <tomeczekstecc@gmail.com>
2024-07-10 07:35:02 +0200

    init

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import {AuthModule} from "./auth/auth.module";
+import { UsersModule } from './users/users.module';
+import { BookmarksModule } from './bookmarks/bookmarks.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, BookmarksModule],
+})
+export class AppModule {}
